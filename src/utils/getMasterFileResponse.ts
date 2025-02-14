1c8f37bffd8719fe2eb8303a882a6fa3f0b9599f
Blissito <fixtergeek@gmail.com>
2025-02-14 09:06:39 -0600

    experimenting with badnwidth

--- src/utils/getMasterFileResponse.ts
+++ src/utils/getMasterFileResponse.ts
@@ -23,18 +23,20 @@ export const getMasterFileString = ({
   versions: VIDEO_SIZE[];
 }) => {
   const HOST = `${CHUNKS_HOST}/${storageKey}`;
-  let content = "#EXTM3U\n";
+  // let content = "#EXTM3U\n";
+  let content = ""; // no first extm3u tag
   if (versions.includes("360p")) {
-    content += `#EXT-X-STREAM-INF:BANDWIDTH=375000,RESOLUTION=640x360\n${HOST}/360p.m3u8\n`;
+    content += `#EXT-X-STREAM-INF:BANDWIDTH=150000,RESOLUTION=640x360\n${HOST}/360p.m3u8\n`;
   }
   if (versions.includes("480p")) {
-    content += `#EXT-X-STREAM-INF:BANDWIDTH=750000,RESOLUTION=854x480\n${HOST}/480p.m3u8\n`;
+    content += `#EXT-X-STREAM-INF:BANDWIDTH=240000,RESOLUTION=854x480\n${HOST}/480p.m3u8\n`;
   }
   if (versions.includes("720p")) {
-    content += `#EXT-X-STREAM-INF:BANDWIDTH=2000000,RESOLUTION=1280x720\n${HOST}/720p.m3u8\n`;
+    content += `#EXT-X-STREAM-INF:BANDWIDTH=440000,RESOLUTION=1280x720\n${HOST}/720p.m3u8\n`;
   }
   if (versions.includes("1080p")) {
-    content += `#EXT-X-STREAM-INF:BANDWIDTH=3500000,RESOLUTION=1920x1080\n${HOST}/1080p.m3u8\n`;
+    content += `#EXT-X-STREAM-INF:BANDWIDTH=640000,RESOLUTION=1920x1080\n${HOST}/1080p.m3u8\n`;
   }
   return content;
+  // badnwidths: https://developer.apple.com/documentation/http-live-streaming/creating-a-multivariant-playlist
 };
